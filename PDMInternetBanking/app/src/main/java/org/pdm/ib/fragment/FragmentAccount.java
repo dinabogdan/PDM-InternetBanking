001dc884be6218681c72ef2750350fab98f652a0
dinabogdan <bogdan.dina03@gmail.com>
2018-03-10 17:42:29 +0200

    refactoring

--- PDMInternetBanking/app/src/main/java/org/pdm/ib/fragment/FragmentAccount.java
+++ PDMInternetBanking/app/src/main/java/org/pdm/ib/fragment/FragmentAccount.java
@@ -9,16 +9,12 @@ import android.view.ViewGroup;
 
 import org.pdm.ib.R;
 
-/**
- * Created by Alex on 10-Mar-18.
- */
-
-public class FragmentGrafic extends Fragment {
+public class FragmentAccount extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_grafic, container, false);
+        return inflater.inflate(R.layout.fragment_account, container, false);
     }
 }
--- PDMInternetBanking/app/src/main/java/org/pdm/ib/fragment/FragmentChart.java
+++ PDMInternetBanking/app/src/main/java/org/pdm/ib/fragment/FragmentChart.java
@@ -9,12 +9,16 @@ import android.view.ViewGroup;
 
 import org.pdm.ib.R;
 
-public class FragmentContCurent extends Fragment {
+/**
+ * Created by Alex on 10-Mar-18.
+ */
+
+public class FragmentChart extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_cont_curent, container, false);
+        return inflater.inflate(R.layout.fragment_chart, container, false);
     }
 }
